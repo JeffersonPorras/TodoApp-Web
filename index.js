292088e824c70bab4ffcd79b10b3e7be0bcbe606
Jefferson <jeffersonporras.jp@gmail.com>
2026-05-18 04:17:52 +0200

    style: estructurado el layout principal y menú con css grid

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 
-const createTitle = (value) =>{
-     
-    const title = document.createElement('h1');
-    title.textContent = value;    
-    return document.body.appendChild(title);
-} 
 
-
-createTitle("My Todo List")
