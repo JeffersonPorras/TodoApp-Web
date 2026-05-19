ce4e14b3b8dad05cd36a86ae210c2aca94e94302
Jefferson <jeffersonporras.jp@gmail.com>
2026-05-19 03:20:00 +0200

    feat: añadida interactividad al menú de navegación con javascript

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const navButtons = document.querySelectorAll('.container__nav-btn');
+const views = document.querySelectorAll('.container__view');
 
 
+navButtons.forEach(button =>{
+    button.addEventListener('click', () => {
+        navButtons.forEach(btn => btn.classList.remove('container__nav-btn--active'));
+        button.classList.add('container__nav-btn--active');
+
+        const targetView = button.getAttribute('data-view');  
+
+        views.forEach(view =>{
+            if (view.id ===`view-${targetView}`) {
+                view.classList.remove('container__view--hidden');
+            } else {
+                view.classList.add('container__view--hidden');
+            }
+        });
+    });
+});
