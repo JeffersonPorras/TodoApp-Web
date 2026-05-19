0d733a6562398c9073cf9d09aa3d869472496bb0
Jefferson <jeffersonporras.jp@gmail.com>
2026-05-19 04:09:11 +0200

    feat: capturada la entrada del usuario y almacenamiento de tareas en objetos

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const containerInput = document.getElementById('form-input');
+const containerBtn = document.getElementById('form-btn');
+const listaDiarias = document.getElementById('list-diarias')
+
+
 const navButtons = document.querySelectorAll('.container__nav-btn');
 const views = document.querySelectorAll('.container__view');
 
+let tasks = [];
 
 navButtons.forEach(button =>{
     button.addEventListener('click', () => {
@@ -18,3 +24,24 @@ navButtons.forEach(button =>{
         });
     });
 });
+
+containerBtn.addEventListener('click', () =>{
+    const taskText = containerInput.value.trim();
+
+    if (taskText === '') return;
+
+    const newtask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false,
+        view:'diarias'
+    };
+
+    tasks.push(newtask);
+
+    containerInput.value = '';
+    containerInput.focus();
+
+    console.log("lista de tareas actuales : ", tasks);
+    
+})
